df7b11afb39ce2875fff4a5d7cf5be3c0ba4903f
熊正奇 <as7196528@gmail.com>
2021-07-22 13:52:02 +0800

    add header fixed

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,21 @@
-console.log('Hello!');
-
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  var targetTop;      //負責隨時偵測取得上方的距離
+  var scrollTime;     //負責視窗捲動時偵測等待的時間
+
+  //判斷往上捲出的高度是否已超過header的高度, 如果header已捲出畫面則加上fixed的class名稱
+  function checkHeader(){
+    if( $(document).scrollTop() >= $('header').height() ){
+      $('header').addClass('fixed');
+    }
+    if( $(document).scrollTop() == 0 ){
+      $('header').removeClass('fixed');
+    }
+  }
+  checkHeader();
+
+  //當視窗捲動時==============================================================================
+  $(window).scroll(function(){
+    clearTimeout(scrollTime);
+    scrollTime = setTimeout(function(){ checkHeader(); },100);
+  });
 });
