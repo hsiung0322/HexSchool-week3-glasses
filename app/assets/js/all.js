f1b642df6d776b7e358ff76a78905799265bed96
熊正奇 <as7196528@gmail.com>
2021-07-23 17:26:28 +0800

    add page/product

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,4 @@
 $(document).ready(() => {
-  var targetTop;      //負責隨時偵測取得上方的距離
   var scrollTime;     //負責視窗捲動時偵測等待的時間
 
   //判斷往上捲出的高度是否已超過header的高度, 如果header已捲出畫面則加上fixed的class名稱
@@ -18,4 +17,19 @@ $(document).ready(() => {
     clearTimeout(scrollTime);
     scrollTime = setTimeout(function(){ checkHeader(); },100);
   });
+
+  $('.tabs a').click(function(){
+      //移除所有active
+      $('.tab').removeClass('active');
+      //當前的元素li加上active
+      $(this).parent().addClass('active');
+
+      //display content
+      let currentTab = $(this).attr('href');
+      $('.product-main>div').hide();
+      $(currentTab).show();
+
+      return false;
+  });
+
 });
